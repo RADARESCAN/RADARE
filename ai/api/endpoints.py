43a5bec556bd018568f704a8d0b7025ec1189c60
FINDXR <eduardermakov769@gmail.com>
2025-11-22 07:01:34 +0800

    Update endpoints.py

--- ai/api/endpoints.py
+++ ai/api/endpoints.py
@@ -1,4 +1,4 @@
-import os        MISCAN
+import os        FINDXR
 import logging
 from typing import Dict, List, Optional, Union
 from fastapi import APIRouter, HTTPException, Depends, status, UploadFile, File
